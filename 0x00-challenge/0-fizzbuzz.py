1608820ae19480649e1bf557678f99487f346588
Oblique000 <neomathe000@gmail.com>
2024-03-19 08:34:10 -0700

    FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+""" FizzBuzz
 """
-FizzBuzz:
-A program that prints numbers from 1 to n, but for multiples of three, prints "Fizz" instead of the number, and for the multiples of five, prints "Buzz". For numbers which are multiples of both three and five, it prints "FizzBuzz".
-"""
-
 import sys
+
 
 def fizzbuzz(n):
     """
@@ -17,37 +15,27 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-    # List to store the results
     tmp_result = []
-
-    # Iterate from 1 to n (inclusive)
-    for i in range(1, n + 1):
-        # Check for multiples of both 3 and 5
-        if i % 3 == 0 and i % 5 == 0:
+    i = 1  # Start value for the while loop
+    while i <= n:
+        if (i % 3) == 0 and (i % 5) == 0:  # Reordered if statements to check for multiples of both 3 and 5 first
             tmp_result.append("FizzBuzz")
-        # Check for multiples of 3
-    elif i % 3 == 0:
-        tmp_result.append("Fizz")
-        # Check for multiples of 5
-    elif i % 5 == 0:
-        tmp_result.append("Buzz")
-        # If none of the above conditions match, just append the number itself
-    else:
-        tmp_result.append(str(i))
-
-    # Join the list elements with space and print
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+        i += 1  # Incrementing the loop counter
     print(" ".join(tmp_result))
 
+
 if __name__ == '__main__':
-    # Check if the script is run with a command-line argument
     if len(sys.argv) <= 1:
         print("Missing number")
-        print("Usage: ./fizzbuzz.py <number>")
-        print("Example: ./fizzbuzz.py 89")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    # Convert the command-line argument to an integer
     number = int(sys.argv[1])
-
-    # Call the fizzbuzz function with the provided number
     fizzbuzz(number)
